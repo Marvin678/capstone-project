b8018b456ce228abb5641f240012b2b32594f743
Marvin Reich <marvin.reich@outlook.de>
2022-10-07 11:58:13 +0200

    add fetch and map function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
 import Head from 'next/head';
+import {useState} from 'react';
 
 import Layout from '../components/Layout';
 import Searchfield from '../components/Searchfield';
 
 export default function HomePage() {
+	const [recipes, setRecipes] = useState([]);
+	console.log(recipes);
 	return (
 		<Layout>
 			<Head>
 				<title key="title">My Project</title>
 				<meta key="description" name="description" content="This is my project" />
 			</Head>
-			<Searchfield />
+
+			<Searchfield onChangeRecipes={setRecipes} />
 		</Layout>
 	);
 }
